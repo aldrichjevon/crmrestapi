02f35f01fd44c3e1e9dd076bab122c1ef04600d0
Jevon <aldrichjevon@gmail.com>
2021-05-29 01:44:48 +0700

    LASTE HARUSNYA

--- connection.js
+++ connection.js
@@ -2,17 +2,17 @@ const mysql = require('mysql');
 
 // Database Connection for Production
 
-// let config = {
-//     user: process.env.SQL_USER,
-//     database: process.env.SQL_DATABASE,
-//     password: process.env.SQL_PASSWORD,
-// }
+let config = {
+    user: process.env.SQL_USER,
+    database: process.env.SQL_DATABASE,
+    password: process.env.SQL_PASSWORD,
+}
 
-// if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
-//   config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
-// }
+if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
+  config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
+}
 
-// let connection = mysql.createConnection(config);
+let connection = mysql.createConnection(config);
 
 // Database Connection for Development
 
